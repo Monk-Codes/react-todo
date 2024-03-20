403b8f8b0dd4b1deb20919c5109293e723621795
monk-codes <pratik120997@gmail.com>
2024-03-20 15:59:20 +0530

    added priority

--- src/components/code/priority.jsx
+++
@@ -1,3 +0,0 @@
-export default function Priority() {
- return <h1>Pri</h1>;
-}
--- src/components/code/todo_form.jsx
+++ src/components/code/todo_form.jsx
@@ -13,7 +13,7 @@ function TodoForm() {
 
  function handleSubmit(e) {
   e.preventDefault();
-  setTodos([inputValue, ...todos]);
+  setTodos([{ text: inputValue, priorityLevel: "low" }, ...todos]);
   setInputValue("");
  }
 
@@ -24,11 +24,19 @@ function TodoForm() {
 
  const handleUpdate = (id) => {
   const newTodos = [...todos];
-  newTodos[id] = editedValue;
+  newTodos[id].text = editedValue;
   setTodos(newTodos);
   setEditIndex(null);
   setEditedValue("");
  };
+ const handlePriority = (id, priorityLevel) => {
+  const newTodos = [...todos];
+  newTodos[id].priorityLevel = priorityLevel;
+  setTodos(newTodos);
+ };
+
+ //  const priorityClasses = ["low", "medium", "high", "urgent"];
+
  return (
   <div className="todo-form-container">
    <form>
@@ -38,25 +46,29 @@ function TodoForm() {
 
    <ul className="todo-list">
     {todos.map((todo, id) => (
-     <li key={id} className="todo-item">
+     <li key={id} className={`todo-item ${todo.priorityLevel}`}>
       {editIndex === id ? (
        <>
         <input type="text" value={editedValue} onChange={(e) => setEditedValue(e.target.value)} />
-
         <button onClick={() => handleUpdate(id)}>✏️</button>
        </>
       ) : (
        <>
-        {todo}
-        <div className="todo-btn">
+        <div className="input">{todo.text}</div>
+
+        <div className="level">
          <button
           onClick={() => {
            setEditIndex(id);
-           setEditedValue(todo);
+           setEditedValue(todo.text);
           }}
          >
           ✏️
          </button>
+         <button onClick={() => handlePriority(id, "low")}>🔵</button>
+         <button onClick={() => handlePriority(id, "medium")}>🟡</button>
+         <button onClick={() => handlePriority(id, "high")}>🟠</button>
+         <button onClick={() => handlePriority(id, "urgent")}>🔴</button>
          <button onClick={() => handleDelete(id)}>X</button>
         </div>
        </>
